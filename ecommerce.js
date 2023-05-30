47a4625fb341db1b98dd8ede08cf6505db1873b3
SuperClase <amarqueznovella@hotmail.com>
2023-05-30 14:19:41 +0200

    carrito

--- ecommerce.js
+++ ecommerce.js
@@ -2,6 +2,8 @@ const botonBuscar = document.querySelector('.btn-buscar');
 const inputBuscar = document.querySelector('.buscar');
 const menu = document.querySelector('.menu');
 const input = document.querySelector('.input');
+const carrito = document.querySelector('.btn-carrito');
+const badge = document.querySelector('.badge');
 
 
 
@@ -11,5 +13,12 @@ botonBuscar.addEventListener('click', () => {
     menu.classList.toggle('buscarActive');
     inputBuscar.classList.toggle('active')
     input.focus();
+
+})
+
+// función solo para desarrollo
+carrito.addEventListener('click', () => {
+    badge.innerHTML = Math.ceil(Math.random() * 10);
+    badge.classList.toggle('oculto');
 
 })
