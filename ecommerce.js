594bf5d898d824f5b773aab034a33204fa641e1c
SuperClase <amarqueznovella@hotmail.com>
2023-06-01 16:01:29 +0200

    Dark mode

--- ecommerce.js
+++ ecommerce.js
@@ -4,7 +4,20 @@ const menu = document.querySelector('.menu');
 const input = document.querySelector('.input');
 const carrito = document.querySelector('.btn-carrito');
 const badge = document.querySelector('.badge');
+const iconosDarkMode = document.querySelector('.iconosDarkMode');
+const body = document.querySelector('body');
+const botonesDarkMode = document.querySelectorAll('.iconoDark')
 
+
+
+
+//  dark mode
+iconosDarkMode.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    botonesDarkMode.forEach((boton) => {
+        boton.classList.toggle('oculto')
+    })
+} )
 
 
 
