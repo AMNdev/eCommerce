af8cb62026f59e1c81a71053285952ae83052618
SuperClase <amarqueznovella@hotmail.com>
2023-06-06 13:21:31 +0200

    boton arriba, colores y mas

--- ecommerce.js
+++ ecommerce.js
@@ -35,3 +35,20 @@ carrito.addEventListener('click', () => {
     badge.classList.toggle('oculto');
 
 })
+
+// Boton Arriba
+const buttonTop = document.querySelector('#buttonTop');
+
+window.onscroll = () => {
+    if (document.documentElement.scrollTop > 100) {
+        buttonTop.classList.add('shows')
+    } else {
+        buttonTop.classList.remove('shows')
+    }
+    buttonTop.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    })
+}
